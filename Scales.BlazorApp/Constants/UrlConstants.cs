96fe08cb327bc68a647119f596a8743453622c84
DENIS <kotdeniss2@yandex.ru>
2023-02-13 15:20:09 +0300

    change urls

--- Scales.BlazorApp/Constants/UrlConstants.cs
+++ Scales.BlazorApp/Constants/UrlConstants.cs
@@ -2,8 +2,8 @@
 {
     public class UrlConstants
     {
-        public const string JOURNAL_URL = "https://31.31.201.154:5002";
-        public const string REFBOOK_URL = "https://31.31.201.154:5003";
+        public const string JOURNAL_URL = "http://31.31.201.154:5002";
+        public const string REFBOOK_URL = "http://31.31.201.154:5003";
         //public const string JOURNAL_URL = "https://mysite.com";
         //public const string REFBOOK_URL = "https://mysite.com";
     }
